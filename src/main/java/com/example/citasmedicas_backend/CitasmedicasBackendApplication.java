e582f9bfe83c3307d7df1aa328dd64879139bf66
BryanArredon <byemilio2005@gmail.com>
2025-10-03 19:04:33 -0600

    Proyecto unicial con springboot

---
+++ src/main/java/com/example/citasmedicas_backend/CitasmedicasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.citasmedicas_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasmedicasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasmedicasBackendApplication.class, args);
+	}
+
+}
