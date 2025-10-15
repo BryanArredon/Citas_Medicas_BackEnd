e4611b68c08d735bfb27c88927c717f791cb6af2
BryanArredon <byemilio2005@gmail.com>
2025-10-14 23:25:00 -0600

    ms pagos con port 8050

---
+++ src/main/java/com/example/citasmedicas_backend/citas/dto/TarjetaDto.java
@@ -0,0 +1,10 @@
+package com.example.citasmedicas_backend.citas.dto;
+
+public class TarjetaDto {
+    private Long id;
+
+    public TarjetaDto() {}
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+}
